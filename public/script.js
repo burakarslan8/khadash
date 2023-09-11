702e8bd8d971a2af956fba107d2771706edec05f
burakarslan8 <rugotr@gmail.com>
2023-09-11 18:05:56 +0000

    data collection

---
+++ public/script.js
@@ -0,0 +1,18 @@
+function fetchData() {
+    fetch('/api/cpu-memory')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('cpu-usage').textContent = `${data.cpu}%`;
+            document.getElementById('memory-usage').textContent = `${data.memory}%`;
+            document.getElementById('cpu-temperature').textContent = `${data.temperature}°C`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+// Fetch data initially
+fetchData();
+
+// Fetch data every 5 seconds
+setInterval(fetchData, 5000);
--- server.js
+++ server.js
@@ -8,3 +8,33 @@ app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+const os = require('os');
+
+const { exec } = require('child_process');
+
+app.get('/api/cpu-memory', (req, res) => {
+    const cpuUsage = os.loadavg()[0]; // Get CPU usage
+    const totalMemory = os.totalmem(); // Get total memory
+    const freeMemory = os.freemem();   // Get free memory
+
+    // Calculate memory usage as a percentage
+    const memoryUsage = ((totalMemory - freeMemory) / totalMemory) * 100;
+
+    // Execute the command to get CPU temperature
+    exec('cat /sys/class/thermal/thermal_zone0/temp', (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error getting CPU temperature:', error);
+            res.status(500).json({ error: 'Failed to get CPU temperature' });
+            return;
+        }
+
+        const cpuTemperature = parseFloat(stdout) / 1000; // Convert to degrees Celsius
+
+        res.json({
+            cpu: cpuUsage.toFixed(2),       // Convert to a fixed number of decimal places
+            memory: memoryUsage.toFixed(2), // Convert to a fixed number of decimal places
+            temperature: cpuTemperature.toFixed(2) // Convert to a fixed number of decimal places
+        });
+    });
+});
